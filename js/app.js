33f7a7c18f0f8f296c8f60c567fd5de8ae548f0f
suelliton <tonmelodicmetal@gmail.com>
2017-03-02 10:53:59 -0300

    commit automatico

--- js/app.js
+++ js/app.js
@@ -1,10 +1,24 @@
-angular.module('books',[]);
-angular.module('books').controller('books_controller',function($scope) {
+angular.module("books",[]);
+angular.module("books").controller("books_controller",function($scope) {
+	$scope.teste = "testando";
 	$scope.books = [
-		{name:'A_Ciência_Secreta.pdf'},
-		{name:'Teoria_das_cordas.pdf'}
+		{name:"A_Ciência_Secreta",pdf:"pdfs/A_Ciência_Secreta.pdf",image:"images/a_ciencia_secreta.jpg"},
+		{name:"Teoria_das_cordas",pdf:"pdfs/Teoria_das_cordas.pdf",image:"images/teoria_das_cordas.jpeg"}
 	];
 
+	$scope.show = function() {
+		$("iframe").attr('width','1200');
+		$("iframe").attr('height','800');
+	}
+	$scope.hide = function() {
+		$("iframe").attr('width','200');
+		$("iframe").attr('height','100');
+	}
+	$scope.clicked = function(book){
+		$scope.book_show = book.pdf;
+		
+	}
+	
 
 
 });
