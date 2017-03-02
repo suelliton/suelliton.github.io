46da617e365badcf47818b59e30a59c420639ce8
suelliton <tonmelodicmetal@gmail.com>
2017-03-02 08:45:05 -0300

    commit automatico

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,5 @@ git add --all
 git status
 git commit -m "commit automatico"
 git push https://github.com/suelliton/suelliton.github.io.git
+suelliton
+suelliton110886
--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-angular.mod
+angular.module('books',[]);
+angular.module('books').controller('books_controller',function($scope) {
+	$scope.books = [
+		{name:'A_Ciência_Secreta.pdf'},
+		{name:'Teoria_das_cordas.pdf'}
+	];
+
+
+
+});
