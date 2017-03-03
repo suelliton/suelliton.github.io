ff1e28d6bfe97f2e45335fc29874047792f6734e
suelliton <tonmelodicmetal@gmail.com>
2017-03-03 20:15:38 -0300

    commit automatico

--- js/app.js
+++ js/app.js
@@ -1,12 +1,52 @@
 angular.module("books",[]);
-angular.module("books").controller("books_controller",function($scope) {
-	$scope.teste = "testando";
-	$scope.books = [
-		{name:"A_Ciência_Secreta",pdf:"pdfs/A_Ciência_Secreta.pdf",image:"images/a_ciencia_secreta.jpg",categoria:'Ciência'},
-		{name:"Teoria_das_cordas",pdf:"pdfs/Teoria_das_cordas.pdf",image:"images/teoria_das_cordas.jpeg",categoria:'Budismo'},
-		{name:"As Guerras de Deuses e Homens",pdf:"pdfs/As Guerras de Deuses e Homens.pdf",image:"images/As Guerras de Deuses e Homens.jpg",categoria:'Ascensão Planetária'},
-		{name:"A Divisão da Terra",pdf:"pdfs/A Divisão da Terra.pdf",image:"images/A Divisão da Terra.jpg",categoria:'Extra-terrestres'}
+ 
+angular.module("books").controller("books_controller",function($scope,$http) {	
+	
+	$scope.carregar = function () {
+	$http.get('https://www.dropbox.com/s/wik2e3uc5hv8o3c/teste.json?dl=1').then(function (success){
+   		console.log("funfou"); 
+   		var jsObj = angular.fromJson(success);  		
+   		$scope.books = jsObj.data.books;
+   		console.log($scope.books);
+   },function (error){
+   		console.log("erro");
+   });
+}
+	
+
+	$scope.carregar();
+	/*
+$scope.books = [
+		{
+			name:"A_Ciência_Secreta",
+			pdf:"pdfs/A_Ciência_Secreta.pdf",
+			image:"images/a_ciencia_secreta.jpg",
+			categoria:'Ciência',
+			autor:"Henri Durville"
+		},
+		{
+			name:"Teoria_das_cordas",
+			pdf:"pdfs/Teoria_das_cordas.pdf",
+			image:"images/teoria_das_cordas.jpeg",
+			categoria:'Budismo',
+			autor:'The Elegante'
+		},
+		{
+			name:"As Guerras de Deuses e Homens",
+			pdf:"pdfs/As Guerras de Deuses e Homens.pdf",
+			image:"images/As Guerras de Deuses e Homens.jpg",
+			categoria:'Ascensão Planetária',
+			autor:'Torres'
+		},
+		{
+			name:"A Divisão da Terra",
+			pdf:"pdfs/A Divisão da Terra.pdf",
+			image:"images/A Divisão da Terra.jpg",
+			categoria:'Extra-terrestres',
+			autor:'Zecharia Sitchin'
+		}
 	];
+*/
 
 	$scope.show = function() {
 		$("iframe").attr('width','100%');
@@ -18,24 +58,15 @@ angular.module("books").controller("books_controller",function($scope) {
 	}
 	$scope.clicked = function(book){
 		$scope.book_show = book;
+		console.log($scope.book_show.name);
 		$("#soundcloud").hide();		
 		setInterval(function() {
 			$("#soundcloud").slideDown();
 		},5000);	
-		var a = setInterval(function () {
-		$(document).keyup(function(event, customEvent){
-    if(typeof customEvent !== 'undefined'){
-        event.which = customEvent.which;
-    }
-    alert(event.which);
-});
-			$('#soundcloud').click(function(){
-  		   $(window).trigger('keyup', {which: 40});
-});
+		var a = setInterval(function () {		
 
-			a();
-		},200);
 			
+		},200);			
 	}
 	
 
