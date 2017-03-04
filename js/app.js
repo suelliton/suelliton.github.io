f4d7fd93e606361f1d5a12369d2ba08c1c312a00
suelliton <tonmelodicmetal@gmail.com>
2017-03-04 09:28:26 -0300

    commit automatico

--- js/app.js
+++ js/app.js
@@ -1,53 +1,48 @@
 var app = angular.module("books",[]);
+angular.module("books").controller("books_controller",function($interval,$timeout,$scope,$http) {	
+	//variaveis de escopo
+	$scope.categoria = "";
+	$scope.carregando = true;
+	$scope.load = 0;
+	//endereço dos jsons
+	home = 'https://www.dropbox.com/s/9346fuuuhahu8m2/home.json?dl=1';
+	transicao = 'https://www.dropbox.com/s/sziguclzzplh2us/transicao.json?dl=1';
+	$scope.carregar = function (cat) {
+		$scope.carregando = true;
+		$scope.load = 0;
+		if(cat == 1){
+			 cat = home; 
+			 $scope.categoria = "Destaques";}
+		if(cat == 2){ 
+			cat = transicao; 
+			$scope.categoria = "Ascensão Planetária";}
 
-angular.module("books").controller("books_controller",function($scope,$http) {		
-
-
-	$scope.carregar = function () {
-	$http.get('https://www.dropbox.com/s/wik2e3uc5hv8o3c/teste.json?dl=1').then(function (success){
-   		console.log("funfou"); 
+	$http.get(cat).then(function (success){
+   		console.log("funfou  "+cat); 
+   		$scope.books = [];
    		var jsObj = angular.fromJson(success);  		
    		$scope.books = jsObj.data.books;
    		console.log($scope.books);
+   	
+   		$interval(function() {
+   			$scope.load = $scope.load + 10;
+   		},200);
+		$timeout(function () {
+   			$scope.carregando = false;
+   		}, 2000);
+
+   		
+
+   	
    },function (error){
    		console.log("erro");
    });
 }
 	
 
-	$scope.carregar();
-	/*
-$scope.books = [
-		{
-			name:"A_Ciência_Secreta",
-			pdf:"pdfs/A_Ciência_Secreta.pdf",
-			image:"images/a_ciencia_secreta.jpg",
-			categoria:'Ciência',
-			autor:"Henri Durville"
-		},
-		{
-			name:"Teoria_das_cordas",
-			pdf:"pdfs/Teoria_das_cordas.pdf",
-			image:"images/teoria_das_cordas.jpeg",
-			categoria:'Budismo',
-			autor:'The Elegante'
-		},
-		{
-			name:"As Guerras de Deuses e Homens",
-			pdf:"pdfs/As Guerras de Deuses e Homens.pdf",
-			image:"images/As Guerras de Deuses e Homens.jpg",
-			categoria:'Ascensão Planetária',
-			autor:'Torres'
-		},
-		{
-			name:"A Divisão da Terra",
-			pdf:"pdfs/A Divisão da Terra.pdf",
-			image:"images/A Divisão da Terra.jpg",
-			categoria:'Extra-terrestres',
-			autor:'Zecharia Sitchin'
-		}
-	];
-*/
+	$scope.carregar(1);
+	
+
 
 	$scope.show = function() {
 		$("iframe").attr('width','100%');
